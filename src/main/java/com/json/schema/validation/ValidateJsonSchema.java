634b8008e1dd495d8cd0354b4364eb5f7c3ad7ec
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-09-27 01:15:04 +0530

    27/09/2024:01:14:00AM

---
+++ src/main/java/com/json/schema/validation/ValidateJsonSchema.java
@@ -0,0 +1,51 @@
+package com.json.schema.validation;
+
+import io.restassured.RestAssured;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import io.restassured.response.Response;
+
+public class ValidateJsonSchema {
+
+	public static void main(String[] args) {
+		
+		var jsonSchema = """
+				{
+				  "$schema": "http://json-schema.org/draft-04/schema#",
+				  "type": "array",
+				  "items": [
+				    {
+				      "type": "object",
+				      "properties": {
+				        "id": {
+				          "type": "integer"
+				        },
+				        "name": {
+				          "type": "string"
+				        },
+				        "slug": {
+				          "type": "string"
+				        },
+				        "sorting": {
+				          "type": "integer"
+				        }
+				      },
+				      "required": [
+				        "id",
+				        "name",
+				        "slug",
+				        "sorting"
+				      ]
+				    }
+				  ]
+				}
+												""";
+		
+		Response response = RestAssured.given()
+		           .when()
+		           .get("https://demostore.gatling.io/api/category");
+		
+		response.then().assertThat().body(JsonSchemaValidator.matchesJsonSchema(jsonSchema));
+
+	}
+
+}
