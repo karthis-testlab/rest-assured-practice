8698446c019606e19c4b863b8c66728e3263c336
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-10-01 21:12:10 +0530

    01/10/2024:09:12:00PM

--- src/main/java/com/mocking/wiremock/practice/APIWireMockForPostCall.java
+++ src/main/java/com/mocking/wiremock/practice/APIWireMockForPostCall.java
@@ -1,10 +1,12 @@
 package com.mocking.wiremock.practice;
 
+import com.github.tomakehurst.wiremock.client.WireMock;
+
 public class APIWireMockForPostCall {
 
 	public static void main(String[] args) {
 		
-		
+		WireMock mockServer = new WireMock(8585);
 
 	}
 
