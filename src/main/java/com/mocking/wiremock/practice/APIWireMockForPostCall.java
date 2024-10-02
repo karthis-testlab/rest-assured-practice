b19e2a299d158d88503be680e791d31be1e08bc0
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-10-03 03:17:08 +0530

    03/10/2024:03:17:00AM

--- src/main/java/com/mocking/wiremock/practice/APIWireMockForPostCall.java
+++ src/main/java/com/mocking/wiremock/practice/APIWireMockForPostCall.java
@@ -9,6 +9,13 @@ public class APIWireMockForPostCall {
 		
 		WireMockServer mockServer = new WireMockServer(8585);
 		mockServer.start();
+		
+		mockServer.stubFor(WireMock.post("/some/endpoint")
+				  .willReturn(WireMock.aResponse()
+						  .withHeader("Content-Type", "application/json")
+						  .withBody("{\"name\": \"Dummy\"}")
+						  .withStatus(201)
+						  .withBody("{\"success\": \"OK\", \"code\": 201}")));
 
 		mockServer.stop();
 	}
