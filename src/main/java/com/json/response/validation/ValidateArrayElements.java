5fbeb630f30dd9fc9653082f1684d36d1cad6c74
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-11-10 00:19:19 +0530

    10/11/2024:12:19:00AM

--- src/main/java/com/json/response/validation/ValidateArrayElements.java
+++ src/main/java/com/json/response/validation/ValidateArrayElements.java
@@ -2,8 +2,7 @@ package com.json.response.validation;
 
 public class ValidateArrayElements {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {		
 
 	}
 
